830e2534e1283c7eccb1e5cbd395a3f9c6f58bc2
KalebGiger <kalebrgiger@gmail.com>
2019-10-27 16:17:45 -0400

    Added my_wc.c to repo

---
+++ my_wc.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]){
+	FILE *fp;
+	int words = 0, chars = 0, lines = 0, i, count = 1, l = 0, w = 0, c = 0;
+	char prev, current;
+
+	for(i = 1; i < argc; i++){
+
+		if(strcmp("-l", argv[i]) == 0){
+			count++;
+			l++;
+		}
+		else if(strcmp("-w", argv[i]) == 0){
+			count++;
+			w++;
+		}
+		else if(strcmp("-c", argv[i]) == 0){
+			count++;
+			c++;
+		}
+		
+		else{
+			fp = fopen(argv[i], "r");
+			while((fscanf(fp, "%c", &current)) != EOF) {
+				chars++;
+				if(current == '\n')
+					lines++;
+				if (isspace(current)) 
+					words++;
+				prev = current;
+			}
+			fclose(fp);
+		}
+	}
+	if (argc == 1) {
+    		printf("my_wc: [option ...] [file ...]\n");   
+   		exit(1);
+  	}
+		
+	if(l == 1)
+		printf("lines: %d ", lines);
+	if(w == 1)
+		printf("words: %d ", words);
+	if(c == 1)
+		printf("characters: %d ", chars);
+	if(l == 1 || w == 1 || c == 1)
+		printf("%s\n", argv[count]);
+	else
+		printf("my_wc: [option ...] [file ...]\n"); 
+
+	return 0;
+	
+}
+
